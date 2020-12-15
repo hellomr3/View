a6425ca1abdafbbf43981bb0fb85600b2f2645eb
mr.3 <1072818264@qq.com>
2020-12-15 11:23:28 +0800

    feat: 1、RecordTimer的实现

--- app/src/main/java/com/looptry/view/MainActivity.kt
+++ app/src/main/java/com/looptry/view/MainActivity.kt
@@ -3,12 +3,24 @@ package com.looptry.view
 import androidx.activity.viewModels
 import com.looptry.architecture.page.BasicActivity
 import com.looptry.architecture.page.DataBindingConfig
+import kotlinx.android.synthetic.main.activity_timer.*
 
 class MainActivity : BasicActivity() {
 
     private val viewModel by viewModels<MainViewModel>()
 
     override fun getDataBindingConfig(): DataBindingConfig {
-        return DataBindingConfig(BR.vm, R.layout.activity_main, viewModel)
+        return DataBindingConfig(BR.vm, R.layout.activity_timer, viewModel)
+            .addBindingParams(BR.click, ClickProxy())
+    }
+
+    inner class ClickProxy {
+        fun start() {
+            timer.startRecord()
+        }
+
+        fun stop() {
+            timer.stopRecord()
+        }
     }
 }
---
+++ library/src/main/java/com/looptry/library/view/RecordTimer.kt
@@ -0,0 +1,123 @@
+package com.looptry.library.view
+
+import android.animation.ObjectAnimator
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.util.AttributeSet
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.lifecycle.LifecycleObserver
+import com.blankj.utilcode.util.TimeUtils
+import com.looptry.library.R
+import java.util.*
+
+/**
+ * Author: mr.3
+ * Date:
+ * Desc:
+ * Modify By:
+ * Modify Date:
+ */
+class RecordTimer(
+    context: Context,
+    attr: AttributeSet?
+) : ConstraintLayout(context, attr), LifecycleObserver {
+
+    companion object {
+        const val TICK = 0x666
+        const val CLEAR = 0x999
+    }
+
+    private var root: View =
+        LayoutInflater.from(context).inflate(R.layout.view_record_timer, this, true)
+
+    private var imageView: ImageView = root.findViewById(R.id.icon)
+
+    private var textView: TextView = root.findViewById(R.id.tick)
+
+    //事件回调
+    var listener: Listener? = null
+
+    //动画
+    private val animator = ObjectAnimator.ofFloat(imageView, "alpha", 0f, 1f).apply {
+        duration = 1000
+    }
+
+    //当前时间
+    var tickTime: Long = 0L
+        set(value) {
+            val format = TimeUtils.millis2String(value - 8 * 60 * 60 * 1000, "HH:mm:ss")
+            field = value
+            textView.text = format
+            if ((value / 1000) % 2 == 0L) {
+                animator.start()
+            } else animator.reverse()
+        }
+
+
+    val mHandler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            when (msg.what) {
+                TICK -> {
+                    tickTime += 1000
+                }
+                CLEAR -> {
+                    tickTime = 0
+                }
+            }
+        }
+    }
+
+    private var timer: Timer? = null
+
+    init {
+        initAttrs(attr)
+
+    }
+
+    private fun initAttrs(attr: AttributeSet?) {
+        val typedArray = context.obtainStyledAttributes(attr, R.styleable.RecordTimer)
+        //设置默认时间
+        typedArray.getInt(R.styleable.RecordTimer_defaultTimeSecond, 0).also {
+            tickTime = it * 1000L
+        }
+        typedArray.recycle()
+    }
+
+    fun startRecord() {
+        if (timer == null) {
+            timer = Timer()
+        }
+        timer?.schedule(object : TimerTask() {
+            override fun run() {
+                val msg = Message()
+                msg.what = TICK
+                mHandler.sendMessage(msg)
+            }
+        }, 0, 1000)
+        listener?.onStart()
+    }
+
+    fun stopRecord() {
+        //清空timer
+        timer?.cancel()
+        timer?.purge()
+        timer = null
+        //重新展示icon
+        animator.start()
+        //回调接口
+        listener?.onStop()
+    }
+
+    interface Listener {
+        fun onStart()
+        fun onStop()
+    }
+}
