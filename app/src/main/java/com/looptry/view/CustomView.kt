fab0eb5875cba7948ab7772243f2bcf14da7bed2
mr.3 <1072818264@qq.com>
2021-01-19 13:58:53 +0800

    fix: 1、依赖远程架构包，便于测试Jenkins

---
+++ app/src/main/java/com/looptry/view/CustomView.kt
@@ -0,0 +1,24 @@
+package com.looptry.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * Author: mr.3
+ * Date:
+ * Desc:
+ * Modify By:
+ * Modify Date:
+ */
+class CustomView(
+    context: Context,
+    private val attrs: AttributeSet?
+) : View(context, attrs) {
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        
+    }
+}
